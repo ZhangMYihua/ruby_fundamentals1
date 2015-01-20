c9924289429cfeb9cd3291a08d58bdb4220b6dfa
zhangmyihua <michaelzhang2@msn.com>
2015-01-20 13:47:51 -0500

    Finished exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,7 @@
-puts "what is your name?"
+puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}"
+puts "How old are you?"
+age = gets.chomp
+birth = 2015 - age.to_i
+puts "You were born in #{birth}"
